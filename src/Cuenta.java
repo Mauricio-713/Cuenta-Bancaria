e0ca6027fe09b4fe1d5c75236d60ffd6eefc7560
Quintero_06 <maoquintero713@gmail.com>
2025-09-01 20:10:18 -0500

    Update Cuenta.java

---
+++ src/Cuenta.java
@@ -0,0 +1,10 @@
+public class Cuenta {
+
+    protected float saldo;
+    protected int numeroConsignaciones = 0;
+    protected int numeroRetiros = 0;
+    protected float tasaAnual;
+    protected float comisionMensual = 0;
+
+    
+}
